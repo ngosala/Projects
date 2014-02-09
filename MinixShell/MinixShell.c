38a91a41409ef8138c8d972def2e809b5764dfdd
Nikhileswar Gosala <ngosala@hawk.iit.edu>
2014-02-09 03:51:55 -0600

    Lots of changes. Working shell

--- MinixShell/MinixShell.c
+++ MinixShell/MinixShell.c
@@ -15,6 +15,7 @@
 char *PROMPT;
 char *PATH;
 char *HOME;
+static char* args[10];
 
 jmp_buf (getinput);
 
@@ -57,6 +58,7 @@ EC InitialiseEnvironment(){
     
     }
     fclose(fptr);
+    // Reading HOME value
        while(1){
     pathstr=strstr(temp,"HOME");
     break;
@@ -77,14 +79,14 @@ EC InitialiseEnvironment(){
      
 
     
-        // Reading Prompt value
+   // Reading Prompt value
      while(1){
     pathstr=strstr(temp,"PROMPT");
     break;
     }
      PROMPT=strtok(pathstr,";.;");
  
-PROMPT=strstr(PROMPT,"N");
+     PROMPT=strstr(PROMPT,"My");
     
 
     SetHomeDir(HOME);
@@ -139,7 +141,7 @@ int createProcess(){
     int PID,FLAG=0;
     PID=fork();
     
-    return FLAG;
+    return PID;
 }
 
 
@@ -162,6 +164,8 @@ void HandleSignal(int c){
         else if (response=='N' || response=='n'){
         signal(SIGINT, HandleSignal);
         longjmp(getinput, 1);
+           // printf("Continue");
+           // printf("%s",PROMPT);
         
         }
         else{
@@ -169,41 +173,223 @@ void HandleSignal(int c){
             printf("Please provide valid response");
         
         }
+       // response==NULL;
         
     }while(response!='Y' && response!='N' && response!='y' && response!='n');
 
 }
 
+/* Function to parse the input command */
+
+void parser(char *readline, char**argv) {
+    
+	while (*readline != '\0') {
+		while (*readline == ' ' || *readline == '\t' || *readline == '\n')
+			*readline++ = '\0';
+		*argv++ = readline;
+		while (*readline != '\0' && *readline != ' ' && *readline != '\t' && *readline != '\n')
+			readline++;
+	}
+	*argv = '\0';
+}
+
+void Redirect(char *command){
+        char *token1;
+	char *token2;
+	int pid, fd;
+	int flag = 1;
+	token1 = strtok(command, "=>");
+	token2 = strtok(NULL, "=>");
+        int i=0,j=0;
+        while (token2[i] == ' ') {
+		j = 0;
+		while (token2[j]) {
+			token2[j] = token2[j + 1];
+			j++;
+		}
+		i++;
+	}
+        
+        
+    
+
+
+}
+
+void piping(char *pipecommand){
+    
+                int fd[2];
+		pid_t pid;
+                char* token1;
+                char* token2[MAX_LENGTH];
+                int count=0;
+                token1 = strtok(pipecommand, "|");
+                while((token2[count]=strtok(NULL, "|")) != NULL) {
+             
+                    count++;
+                }
+                
+                
+                int i=0,j=0;
+                while (token2[i] == ' ') {
+		j = 0;
+		while (token2[j]) {
+			token2[j] = token2[j + 1];
+			j++;
+		}
+		i++;
+	}
+                
+                
+                
+                
+                
+                
+		/* create the pipe */
+		if (pipe(fd) == -1) {
+			printf("Pipe failed");
+		}
+		/* fork a child process */
+		pid = fork();
+		if (pid < 0) { 
+			printf("Fork Failed");
+			return 1;
+		}
+               printf("%s",token2[1]);
+                
+
+
+}
+
+void ExecuteCommand(char *command){
+FILE *pipein_fp;
+FILE* pipeout_fp[256];
+
+    if(fork()==0){
+        
+        if(strstr(command,"=>")!=NULL){
+        
+            Redirect(command);
+        
+        }
+        else{
+        
+               args[0]="\0";    
+   
+        int i=execvp(command,args);
+        
+         if(i<0){
+        
+            printf("Some Error");
+            exit(1);
+        
+        }
+        else
+        {
+             wait(NULL);
+        }
+
+        }
+    
+
+
+    }
+}
+void executeCdCommand(char *argv[]){
+    
+   
+    int changedir;
+    char* curwd[MAX_LENGTH];
+    getcwd(curwd,MAX_LENGTH);
+    changedir=chdir(curwd);
+    if(changedir!=0){
+        printf("couldnt change dir1");
+    }
+   
+    
+  
+    changedir=chdir(argv[1]);
+    if(changedir!=0){
+        printf("couldnt change dir2");
+    }
+    
+    
+    
+}
+void executePipeCommand(char *readline){
+
+
+}
 int main(int argc, char *argv[], char *envp[])
 {
     char readline[MAX_LENGTH];
     
        //  char str[]="nIkHeLeL";
   //  printf("%s",Lower(str));
-    printf("\nWelcome to Custom Shell\n\n");
+   
+        
+        //clear the window
+	if(fork() == 0) {
+			execv("/usr/bin/clear", argv);
+			exit(1);
+		}
+	else wait(NULL);
+        
+            printf("\nWelcome to Custom Shell\n\n");
 	printf("Nikhileswar Gosala\n");
 	printf("Geetha Sankineni\n");
         printf("Harshitha Bandlam\n");
     
         InitialiseEnvironment();
-        
-        
-        
-       
         setjmp(getinput);
         
        signal(SIGINT,HandleSignal);
         
-       printf("%s", PROMPT);
+     //  printf("%s", PROMPT);
         
        while(1){
+         //  sleep(1);
+     printf("%s",PROMPT); 
+              gets(readline);
+                 
+                 // puts(readline);
+                        
+                  fflush(stdin);
+                  
+			parser(readline, argv);
+                       
+			if (strcmp(argv[0], "exit") == 0) {
+                            
+				printf("\n GoodBye...\n");
+				exit(0);
+			}
+            
+			else 			
+			if (strstr(readline, ">") != NULL) {
+                            Redirect(readline);
+			} else if (strcmp(readline, "getpid") == 0){
+				int pid=createProcess();
+                        printf("%d",pid);
+                        }
+			else if (strstr(readline, "|") != NULL)
+				executePipeCommand(readline);
+			else if (strstr(readline,"cd")!=NULL)
+            {
+                executeCdCommand(argv);
+            }
+                        
+                        else
+                            ExecuteCommand(readline);
+
+                        
        
-           getchar();
-       
+                        	
+                        
+                    
        
        }
-	        
-	   
+	 
+	 
 }
 
 
